c7d818562e4420719d5cd45a79c8986b3e40b5c8
cemreboz <112690868+cemreboz@users.noreply.github.com>
2024-11-12 22:14:37 -0500

    added some entities

---
+++ src/main/java/entity/Persona.java
@@ -0,0 +1,14 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * An implementation of the Persona interface.
+ */
+public class Persona {
+    private int personaId;
+    private String name;
+    private String ageRange;
+    private List<String> interests;
+    private List<String> personalityTraits;
+}
---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * An implementation of the Product interface.
+ */
+public class Product {
+    private int productId;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private Persona targetAudiencePersona;
+    private List<byte[]> images;
+
+}
