28e2ad30fe0e1d21f6c2e966a6cee37f6b873afe
rainychen <rainy.chen@mail.utoronto.ca>
2024-11-27 18:41:28 -0500

    created Detailed Target Audience Presenter

---
+++ src/main/java/interface_adapter/DetailedTargetAudiencePresenter.java
@@ -0,0 +1,43 @@
+package interface_adapter;
+
+import java.util.List;
+
+import entity.DetailedTargetAudience;
+import use_case.set_targetaudience.DetailedInteractor;
+
+/**
+ * Class for the Detailed TA presenter.
+ */
+public class DetailedTargetAudiencePresenter {
+    private final DetailedTargetAudienceView view;
+    private final DetailedInteractor interactor;
+
+    public DetailedTargetAudiencePresenter(DetailedTargetAudienceView view, DetailedInteractor interactor) {
+        this.view = view;
+        this.interactor = interactor;
+    }
+
+    /**
+     * Method for fetching the Detailed TA.
+     * @param audienceCategory the audience category that the detailed TA is based off of.
+     * @throws Exception when there is an error.
+     * @throws IllegalArgumentException when the Audience Category is null/empty.
+     */
+    public void fetchDetailedTargetAudience(String audienceCategory) throws Exception {
+        view.showLoading();
+
+        // Validate input
+        if (audienceCategory == null || audienceCategory.isEmpty()) {
+            throw new IllegalArgumentException("Audience category cannot be null or empty.");
+        }
+
+        // Fetch detailed target audience from the Interactor
+        final List<DetailedTargetAudience> detailedTargetAudience =
+                interactor.fetchDetailedTargetAudience(audienceCategory);
+
+        // Update the View
+        view.hideLoading();
+        view.displayDetailedTargetAudience(detailedTargetAudience);
+
+    }
+}
---
+++ src/main/java/interface_adapter/DetailedTargetAudienceView.java
@@ -0,0 +1,32 @@
+package interface_adapter;
+
+import java.util.List;
+
+import entity.DetailedTargetAudience;
+
+/**
+ * Interface for Detailed Target Audience View.
+ */
+public interface DetailedTargetAudienceView {
+    /**
+     * Method for showLoading.
+     */
+    void showLoading();
+
+    /**
+     * Method for hideLoading.
+     */
+    void hideLoading();
+
+    /**
+     * Method for Display Detailed Target Audience.
+     * @param detailedTargetAudience the input for detailed Target Audience.
+     */
+    void displayDetailedTargetAudience(List<DetailedTargetAudience> detailedTargetAudience);
+
+    /**
+     * Method for showing the error.
+     * @param errorMessage ther error message input.
+     */
+    void showError(String errorMessage);
+}
