74646987447fe8ece281d368eda8a693982ffdee
rainychen <rainy.chen@mail.utoronto.ca>
2024-11-30 16:40:20 -0500

    changing output data

--- src/main/java/use_case/set_targetaudience/DetailedOutputData.java
+++ src/main/java/use_case/set_targetaudience/DetailedOutputData.java
@@ -6,24 +6,15 @@ import entity.DetailedTargetAudience;
  * Class for DetailedTA OutputData.
  */
 public class DetailedOutputData {
+
     private final DetailedTargetAudience detailedTargetAudience;
-    private final String errorMessage;
 
     public DetailedOutputData(DetailedTargetAudience detailedTargetAudience) {
         this.detailedTargetAudience = detailedTargetAudience;
-        this.errorMessage = null;
-    }
-
-    public DetailedOutputData(String errorMessage) {
-        this.detailedTargetAudience = null;
-        this.errorMessage = errorMessage;
     }
 
     public DetailedTargetAudience getDetailedTargetAudience() {
         return detailedTargetAudience;
     }
 
-    public String getErrorMessage() {
-        return errorMessage;
-    }
 }
