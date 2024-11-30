4e24cf18c12fb62340a15b48c9f1bd206a4d3e37
vdomash <viktor.domashnev@gmail.com>
2024-11-30 12:42:53 -0500

    changed state to include error message

--- src/main/java/interface_adapter/compare_personas/ComparePersonasPresenter.java
+++ src/main/java/interface_adapter/compare_personas/ComparePersonasPresenter.java
@@ -2,14 +2,8 @@ package interface_adapter.compare_personas;
 
 import use_case.compare_personas.ComparePersonasOutputBoundary;
 import use_case.compare_personas.ComparePersonasOutputData;
-import interface_adapter.compare_personas.ComparePersonasViewModel;
 
-/**
- * The Presenter for the Compare Personas use case.
- * This class takes output data and transforms it into a format suitable for the view model.
- */
 public class ComparePersonasPresenter implements ComparePersonasOutputBoundary {
-
     private final ComparePersonasViewModel viewModel;
 
     public ComparePersonasPresenter(ComparePersonasViewModel viewModel) {
@@ -17,17 +11,29 @@ public class ComparePersonasPresenter implements ComparePersonasOutputBoundary {
     }
 
     @Override
-    public void prepareSuccessView(ComparePersonasOutputData outputData) {
-        // Update the view model with the result of the comparison
-        viewModel.setSimilarities(outputData.getSimilarities());
-        viewModel.setDifferences(outputData.getDifferences());
-        viewModel.firePropertyChanged(); // Notify view that data has been updated
+    public void presentComparison(ComparePersonasOutputData outputData) {
+        ComparePersonasState state = viewModel.getState();
+
+        state.setPersona1(outputData.getPersona1());
+        state.setPersona2(outputData.getPersona2());
+        state.setPersona1Opinion(outputData.getPersona1Opinion());
+        state.setPersona2Opinion(outputData.getPersona2Opinion());
+        state.setSimilarities(outputData.getSimilarities());
+        state.setDifferences(outputData.getDifferences());
+
+        viewModel.setState(state);
+        viewModel.firePropertyChanged();
     }
 
     @Override
     public void prepareFailView(String errorMessage) {
-        // Update the view model with an error message
-        viewModel.setErrorMessage(errorMessage);
-        viewModel.firePropertyChanged(); // Notify view that data has been updated
+        ComparePersonasState state = viewModel.getState();
+
+        // Set an error message in the state
+        state.setErrorMessage(errorMessage);
+
+        // Update the view model state
+        viewModel.setState(state);
+        viewModel.firePropertyChanged();
     }
 }
--- src/main/java/interface_adapter/compare_personas/ComparePersonasState.java
+++ src/main/java/interface_adapter/compare_personas/ComparePersonasState.java
@@ -1,61 +1,75 @@
 package interface_adapter.compare_personas;
 
 import entity.Persona;
+import java.util.List;
 
+/**
+ * State for the Compare Personas View Model.
+ */
 public class ComparePersonasState {
     private Persona persona1;
     private Persona persona2;
     private String persona1Opinion;
     private String persona2Opinion;
-    private String similarities;
-    private String differences;
+    private List<String> similarities;
+    private List<String> differences;
+    private String errorMessage;
 
-    // Getters and Setters
+    // Getters
     public Persona getPersona1() {
         return persona1;
-    }
-
-    public void setPersona1(Persona persona1) {
-        this.persona1 = persona1;
     }
 
     public Persona getPersona2() {
         return persona2;
     }
 
-    public void setPersona2(Persona persona2) {
-        this.persona2 = persona2;
-    }
-
     public String getPersona1Opinion() {
         return persona1Opinion;
-    }
-
-    public void setPersona1Opinion(String persona1Opinion) {
-        this.persona1Opinion = persona1Opinion;
     }
 
     public String getPersona2Opinion() {
         return persona2Opinion;
     }
 
+    public List<String> getSimilarities() {
+        return similarities;
+    }
+
+    public List<String> getDifferences() {
+        return differences;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    // Setters
+    public void setPersona1(Persona persona1) {
+        this.persona1 = persona1;
+    }
+
+    public void setPersona2(Persona persona2) {
+        this.persona2 = persona2;
+    }
+
+    public void setPersona1Opinion(String persona1Opinion) {
+        this.persona1Opinion = persona1Opinion;
+    }
+
     public void setPersona2Opinion(String persona2Opinion) {
         this.persona2Opinion = persona2Opinion;
     }
 
-    public String getSimilarities() {
-        return similarities;
-    }
-
-    public void setSimilarities(String similarities) {
+    public void setSimilarities(List<String> similarities) {
         this.similarities = similarities;
     }
 
-    public String getDifferences() {
-        return differences;
+    public void setDifferences(List<String> differences) {
+        this.differences = differences;
     }
 
-    public void setDifferences(String differences) {
-        this.differences = differences;
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
